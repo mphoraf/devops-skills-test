61fcc24c2802033902f4c498567439ec310248ac
mphoraf <mphoraf@gmail.com>
2020-11-08 13:59:49 +0200

    added print logic to script

--- listit.sh
+++ listit.sh
@@ -3,3 +3,12 @@
 pwd -P > directories.list
 sed -i "i line one's line" directories.list
 head -n3 directories.list
+
+COUNT=0
+if [ -n "$1" ] && [ $1 -gt 0 ]
+then
+  for COUNT in `seq 1 $1`; do
+    head -n3 directories.list
+  done
+fi
+
